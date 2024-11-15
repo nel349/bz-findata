51ec872e902e1191d361ad96c3f88da41d66e936
Norman Lopez <noell.lpz@gmail.com>
2024-11-15 15:44:16 -0700

    Implemented DecodeSwapExactTokensForETHSupportingFeeOnTransferTokens method id 0x791ac947 and fixed tests

--- internal/dex/eth/uniswap/decoder/decoder.go
+++ internal/dex/eth/uniswap/decoder/decoder.go
@@ -6,6 +6,7 @@ import (
 	"math"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/jmoiron/sqlx"
 	"github.com/nel349/bz-findata/internal/dex/eth/defi_llama"
 	"github.com/nel349/bz-findata/internal/dex/eth/uniswap/v2"
@@ -35,17 +36,17 @@ func DecodeSwap(data []byte, version string, db *sqlx.DB, tokenInfo *defi_llama.
 	switch swapMethod := swapMethod.(type) {
 	case v2.UniswapV2SwapMethod:
 		fmt.Println("V2 swap method")
-        // Lets do a switch for all the v2 swap methods
-        switch swapMethod {
-            case v2.SwapExactTokensForTokens:
-                return DecodeSwapExactTokensForTokens(data, version, tokenInfo)
-            case v2.SwapTokensForExactTokens:
-                return DecodeSwapExactTokensForTokens(data, version, tokenInfo)
-            // Add more cases for other v2 swap methods as needed
-        }
+		// Lets do a switch for all the v2 swap methods
+		switch swapMethod {
+		case v2.SwapExactTokensForTokens:
+			return DecodeSwapExactTokensForTokens(data, version, tokenInfo)
+		case v2.SwapTokensForExactTokens:
+			fmt.Println("not supported yet")
+			// Add more cases for other v2 swap methods as needed
+		}
 
 	case v3.UniswapV3Method:
-		fmt.Println("V3 swap method")
+		fmt.Println("not supported yet")
 	}
 
 	// Debug prints
@@ -54,8 +55,9 @@ func DecodeSwap(data []byte, version string, db *sqlx.DB, tokenInfo *defi_llama.
 	return &entity.SwapTransaction{}, nil
 }
 
-/** function for SwapExactTokensForTokens
-// e.g 
+/*
+* function for SwapExactTokensForTokens
+// e.g
 Function: swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] path, address to, uint256 deadline)
 
 MethodID: 0x38ed1739
@@ -69,56 +71,109 @@ MethodID: 0x38ed1739
 [7]:  000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
 */
 func DecodeSwapExactTokensForTokens(data []byte, version string, tokenInfo *defi_llama.TokenInfo) (*entity.SwapTransaction, error) {
-    // Skip first 4 bytes (method ID)
-    data = data[4:]
-    
-    // [0] amountIn (uint256)
-	fmt.Printf("Raw amountIn bytes: %x\n", data[:32])
-    amountIn := new(big.Int).SetBytes(data[:32])
-	fmt.Printf("Amount In: %v\n", amountIn)
+	// Skip first 4 bytes (method ID)
+	data = data[4:]
 
-    amountInDecimal := new(big.Float).Quo(
-        new(big.Float).SetInt(amountIn),
-        new(big.Float).SetFloat64(math.Pow(10, float64(tokenInfo.Decimals))),
-    )
+	// [0] amountIn (uint256)
+	fmt.Printf("Raw amountIn bytes: %x\n", data[:32])
+	amountIn := new(big.Int).SetBytes(data[:32])
+	// fmt.Printf("Amount In: %v\n", amountIn)
+
+	amountInDecimal := new(big.Float).Quo(
+		new(big.Float).SetInt(amountIn),
+		new(big.Float).SetFloat64(math.Pow(10, float64(tokenInfo.Decimals))),
+	)
 	amountInFloat64, _ := amountInDecimal.Float64()
-    
-    // [1] amountOutMin (uint256)
-    amountOutMin := new(big.Int).SetBytes(data[32:64])
+
+	// [1] amountOutMin (uint256)
+	amountOutMin := new(big.Int).SetBytes(data[32:64])
 	amountOutMinFloat64, _ := new(big.Float).Quo(
 		new(big.Float).SetInt(amountOutMin),
 		new(big.Float).SetFloat64(math.Pow(10, float64(tokenInfo.Decimals))),
 	).Float64()
-    
-    // [2] path offset (points to [5])
-    // offset := new(big.Int).SetBytes(data[64:96])
-    
-    // [3] to address (address)
-    toAddress := fmt.Sprintf("%x", data[96:128])[24:] // Take last 20 bytes
-    
-    // [4] deadline (uint256)
-    // deadline := new(big.Int).SetBytes(data[128:160])
-    
-    // [5] path length
-    // pathOffset := 160  // 5 * 32 bytes
-    // pathLength := new(big.Int).SetBytes(data[160:192]).Uint64()
-    
+
+	// [2] path offset (points to [5])
+	// offset := new(big.Int).SetBytes(data[64:96])
+
+	// [3] to address (address)
+	toAddress := fmt.Sprintf("%x", data[96:128])[24:] // Take last 20 bytes
+
+	// [4] deadline (uint256)
+	// deadline := new(big.Int).SetBytes(data[128:160])
+
+	// [5] path length
+	// pathOffset := 160  // 5 * 32 bytes
+	// pathLength := new(big.Int).SetBytes(data[160:192]).Uint64()
+
+	// [6] and [7] are token addresses in the path
+	tokenPathFrom := fmt.Sprintf("0x%s", common.Bytes2Hex(data[192:224])[24:]) // First token in path
+	tokenPathTo := fmt.Sprintf("0x%s", common.Bytes2Hex(data[224:256])[24:])   // Second token in path
+
+	fmt.Printf("Amount In: %v tokens\n", amountInFloat64)
+	fmt.Printf("To Address: 0x%s\n", toAddress)
+	fmt.Printf("Token Path:\n")
+	fmt.Printf("  From: 0x%s\n", tokenPathFrom)
+	fmt.Printf("  To: 0x%s\n", tokenPathTo)
+
+	return &entity.SwapTransaction{
+		AmountIn:     amountInFloat64,
+		AmountOutMin: amountOutMinFloat64,
+		ToAddress:    toAddress,
+		// Deadline:      deadline.String(),
+		TokenPathFrom: tokenPathFrom,
+		TokenPathTo:   tokenPathTo,
+	}, nil
+}
+
+
+/*
+	Function: swapExactTokensForETHSupportingFeeOnTransferTokens(uint256 amountIn, uint256 amountOutMin, address[] path, address to, uint256 deadline)
+
+	MethodID: 0x791ac947
+	[0]:  0000000000000000000000000000000000000000000422ca8b0a00a424ffffff
+	[1]:  00000000000000000000000000000000000000000000000007c28167a0c85474
+	[2]:  00000000000000000000000000000000000000000000000000000000000000a0
+	[3]:  0000000000000000000000001ad0eb3d4e0b79c20f8b3af24b706ae3c8e6a201
+	[4]:  000000000000000000000000000000000000000000000000000000006736f2f3
+	[5]:  0000000000000000000000000000000000000000000000000000000000000002
+	[6]:  000000000000000000000000f3c7cecf8cbc3066f9a87b310cebe198d00479ac
+	[7]:  000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+	[0]: 32 bytes - amountIn (uint256)
+	[1]: 32 bytes - amountOutMin (uint256)
+	[2]: 32 bytes - path array offset (uint256)
+	[3]: 32 bytes - to address (20 bytes + 12 bytes padding)
+	[4]: 32 bytes - deadline (uint256)
+	[5]: 32 bytes - path array length (uint256)
+	[6]: 32 bytes - first token address (20 bytes + 12 bytes padding)
+	[7]: 32 bytes - second token address (20 bytes + 12 bytes padding)
+*/
+func DecodeSwapExactTokensForETHSupportingFeeOnTransferTokens(
+	data []byte,
+	version string, 
+	tokenInfo *defi_llama.TokenInfo,
+) (*entity.SwapTransaction, error) {
+
+	data = data[4:]
+
+	amountIn := new(big.Int).SetBytes(data[:32])
+	amountInFloat64, _ := new(big.Float).Quo(
+		new(big.Float).SetInt(amountIn),
+		new(big.Float).SetFloat64(math.Pow(10, float64(tokenInfo.Decimals))),
+	).Float64()
+
+	// [5] is where the path array starts (160 bytes from start)
     // [6] and [7] are token addresses in the path
-    tokenPathFrom := fmt.Sprintf("%x", data[192:224])[24:] // First token in path
-    tokenPathTo := fmt.Sprintf("%x", data[224:256])[24:]   // Second token in path
+    tokenPathFrom := fmt.Sprintf("0x%s", common.Bytes2Hex(data[192:224])[24:]) // First token in path
+    tokenPathTo := fmt.Sprintf("0x%s", common.Bytes2Hex(data[224:256])[24:])   // Second token in path
 
-    fmt.Printf("Amount In: %v tokens\n", amountInDecimal)
-    fmt.Printf("To Address: 0x%s\n", toAddress)
-    fmt.Printf("Token Path:\n")
-    fmt.Printf("  From: 0x%s\n", tokenPathFrom)
-    fmt.Printf("  To: 0x%s\n", tokenPathTo)
+	fmt.Printf("Amount In: %v tokens\n", amountInFloat64)
+	fmt.Printf("Token Path:\n")
+	fmt.Printf("  From: 0x%s\n", tokenPathFrom)
+	fmt.Printf("  To: 0x%s\n", tokenPathTo)
 
-    return &entity.SwapTransaction{
-        AmountIn:      amountInFloat64,
-        AmountOutMin:  amountOutMinFloat64,
-        ToAddress:     toAddress,
-        // Deadline:      deadline.String(),
-        TokenPathFrom: tokenPathFrom,
-        TokenPathTo:   tokenPathTo,
-    }, nil
+	return &entity.SwapTransaction{
+		AmountIn: amountInFloat64,
+		TokenPathFrom: tokenPathFrom,
+		TokenPathTo:   tokenPathTo,
+	}, nil
 }
--- internal/dex/eth/uniswap/decoder/decoder_test.go
+++ internal/dex/eth/uniswap/decoder/decoder_test.go
@@ -3,6 +3,8 @@ package decoder
 import (
 	"fmt"
 	"math"
+	"math/big"
+	"strings"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -23,23 +25,13 @@ func TestDecodeSwapExactTokensForTokens(t *testing.T) {
 
 	t.Run("Test DecodeSwapExactTokensForTokens", func(t *testing.T) {
 		swapTransaction, err := DecodeSwapExactTokensForTokens(data, version, tokenInfo)
-
-		if err != nil {
-			t.Errorf("Error decoding swap: %v", err)
-			return
-		}
-
+		checkSwapNotNil(t, err, swapTransaction)
 		expected := &entity.SwapTransaction{
 			AmountIn: 74542093.747294688,
-			// ToAddress:     "0x699ec925118567b6475fe495327ba0a778234aaa",
-			// TokenPathFrom: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-			// TokenPathTo:   "0x699ec925118567b6475fe495327ba0a778234aaa",
+			TokenPathFrom: "0x699Ec925118567b6475Fe495327ba0a778234AaA",
+			TokenPathTo:   "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
 		}
 
-		if swapTransaction == nil {
-			t.Errorf("Swap Transaction is nil")
-			return
-		}
 		fmt.Printf("Swap Transaction: %v\n", swapTransaction)
 
 		// Compare with small epsilon to account for floating point precision
@@ -47,6 +39,70 @@ func TestDecodeSwapExactTokensForTokens(t *testing.T) {
 			t.Errorf("Amount In does not match expected value %v, got %v", expected.AmountIn, swapTransaction.AmountIn)
 		}
 
+		if toLowerCaseHex(expected.TokenPathFrom) != toLowerCaseHex(swapTransaction.TokenPathFrom) {
+			t.Errorf("Token Path From does not match expected value %v, got %v", expected.TokenPathFrom, swapTransaction.TokenPathFrom)
+		}
+
+		if toLowerCaseHex(expected.TokenPathTo) != toLowerCaseHex(swapTransaction.TokenPathTo) {
+			t.Errorf("Token Path To does not match expected value %v, got %v", expected.TokenPathTo, swapTransaction.TokenPathTo)
+		}
 	})
 
+	t.Run("Test DecodeSwapExactTokensForETHSupportingFeeOnTransferTokens", func(t *testing.T) {
+
+		data = common.FromHex("0x791ac9470000000000000000000000000000000000000000000422ca8b0a00a424ffffff00000000000000000000000000000000000000000000000007c28167a0c8547400000000000000000000000000000000000000000000000000000000000000a00000000000000000000000001ad0eb3d4e0b79c20f8b3af24b706ae3c8e6a201000000000000000000000000000000000000000000000000000000006736f2f30000000000000000000000000000000000000000000000000000000000000002000000000000000000000000f3c7cecf8cbc3066f9a87b310cebe198d00479ac000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")
+		tokenInfo := &defi_llama.TokenInfo{
+			Address:  "0xF3c7CECF8cBC3066F9a87b310cEBE198d00479aC",
+			Decimals: 18,
+			Symbol:   "FEG",
+		}
+
+		swapTransaction, err := DecodeSwapExactTokensForETHSupportingFeeOnTransferTokens(data, version, tokenInfo)
+
+		checkSwapNotNil(t, err, swapTransaction)
+		rawAmount, ok := new(big.Int).SetString("4999999999999999999999999", 10) // got expected amount from tenderly dev mode
+		// https://dashboard.tenderly.co/tx/mainnet/0x708b5ce2f7a6e6bf95ed92206955afddfe226bbd1911ff31f1dc9604a25fd93d
+
+		if !ok {
+			t.Fatal("failed to parse big.Int")
+		}
+
+		expectedAmountInFloat64, _ := new(big.Float).Quo(
+			new(big.Float).SetInt(rawAmount),
+			new(big.Float).SetFloat64(math.Pow(10, float64(tokenInfo.Decimals))),
+		).Float64()
+
+		expected := &entity.SwapTransaction{
+			AmountIn: expectedAmountInFloat64,
+			TokenPathFrom: "0xF3c7CECF8cBC3066F9a87b310cEBE198d00479aC",
+			TokenPathTo:   "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+		}
+		if math.Abs(expected.AmountIn-swapTransaction.AmountIn) > 0.000001 {
+			t.Errorf("Amount In does not match expected value %v, got %v", expected.AmountIn, swapTransaction.AmountIn)
+		}
+
+		if toLowerCaseHex(expected.TokenPathFrom) != toLowerCaseHex(swapTransaction.TokenPathFrom) {
+			t.Errorf("Token Path From does not match expected value %v, got %v", expected.TokenPathFrom, swapTransaction.TokenPathFrom)
+		}
+
+		if toLowerCaseHex(expected.TokenPathTo) != toLowerCaseHex(swapTransaction.TokenPathTo) {
+			t.Errorf("Token Path To does not match expected value %v, got %v", expected.TokenPathTo, swapTransaction.TokenPathTo)
+		}
+	})
+
+}
+
+func checkSwapNotNil(t *testing.T, err error, swapTransaction *entity.SwapTransaction) {
+	if err != nil {
+		t.Errorf("Error decoding swap: %v", err)
+		return
+	}
+	if swapTransaction == nil {
+		t.Errorf("Swap Transaction is nil")
+	}
+}
+
+// toLowerCaseHex converts a hex string to lowercase
+func toLowerCaseHex(hex string) string {
+	return strings.ToLower(hex)
 }
